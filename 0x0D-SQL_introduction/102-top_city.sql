eaaa24ca0c14cfc96eb1f89dd0c68176344c8274
Natnael Hailu <122819655+nati673@users.noreply.github.com>
2023-08-15 13:46:29 -0700

    Update 102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,5 +1,6 @@
--- Displays the max temperature of each state, ordered by state name.
-SELECT `state`, MAX(`value`) AS `max_temp`
-FROM `temperatures`
-GROUP BY `state`
-ORDER BY `state`;
+SELECT city, AVG(temperature) AS avg_temp
+FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
