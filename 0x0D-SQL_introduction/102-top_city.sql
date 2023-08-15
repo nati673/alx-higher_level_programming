4e74aa766807e1f58e962ba6c1a95ac797d0ec2f
Natnael Hailu <122819655+nati673@users.noreply.github.com>
2023-08-15 13:59:49 -0700

    Update 102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,10 @@
-SELECT city, AVG(temperature) AS avg_temp
-FROM temperatures
-WHERE month = 7 OR month = 8
+-- displays average temp by city for july and august in descending order
+CREATE TABLE IF NOT EXISTS temp_july_aug
+       SELECT *
+       FROM temperatures
+       WHERE month = 7 OR month = 8;
+SELECT city, AVG(value) AS avg_temp
+FROM temp_july_aug
 GROUP BY city
 ORDER BY avg_temp DESC
-LIMIT 3;
+limit 3;
